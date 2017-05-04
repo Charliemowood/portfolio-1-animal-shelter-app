128b242186e15d276fe86f51163919e5e4fac288
Charlie Wood <charliemowood@gmail.com>
2017-05-04 18:30:57 +0100

    Add class SQLRunner.

---
+++ db/sqlrunner.rb
@@ -0,0 +1,14 @@
+require('pg')
+
+class SqlRunner
+
+  def SqlRunner.run(sql)
+    begin
+      db = PG.connect({dbname: 'animal_shelter', host: 'localhost'})
+      db.exec(sql)
+    ensure
+      db.close()
+    end
+  end
+  
+end
