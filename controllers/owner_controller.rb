7848a8c3ae8e2918bb2c1b0d7835223c638b009b
Charlie Wood <charliemowood@gmail.com>
2017-05-08 17:05:42 +0100

    Add form for changing owner

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -7,3 +7,16 @@ get '/owners' do
   @owners = Owner.all()
   erb (:"owner/index")
 end
+
+#NEW - CREATE (get form)
+get '/owners/new' do
+  @cats = Cat.all()
+  @owners = Owner.all()
+  erb (:"owner/new")
+end
+
+post "/owners" do
+  @owner = Cat.new(params)
+  @owner.save()
+  erb(:create)
+end
