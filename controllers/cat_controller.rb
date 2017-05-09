18960e78daed01f21b642a32ef6940bbb70a46c5
Charlie Wood <charliemowood@gmail.com>
2017-05-09 20:31:09 +0100

    Add create new cat with get and post

--- controllers/cat_controller.rb
+++ controllers/cat_controller.rb
@@ -1,8 +1,20 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/cat.rb')
+require_relative('../models/owner.rb')
 
 get '/cats' do
   @cats = Cat.all()
   erb(:"cat/index")
+end
+
+get '/cats/new' do
+  @owners = Owner.all()
+  erb(:"cat/new")
+end
+
+post '/cats' do
+  @cat = Cat.new(params)
+  @cat.save()
+  erb (:"cat/create")
 end
