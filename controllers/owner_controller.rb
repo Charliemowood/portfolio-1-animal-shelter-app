26d3b716ac89ea75b9d9e9a83fb071fa236eb962
Charlie Wood <charliemowood@gmail.com>
2017-05-09 18:38:36 +0100

    Cleanup codebase ready for reimplimentation of restful paths

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -8,22 +8,8 @@ get '/owners' do
   erb (:"owner/index")
 end
 
-#NEW - CREATE (get form)
 get '/owners/new' do
   @cats = Cat.all()
   @owners = Owner.all()
   erb (:"owner/new")
 end
-
-# post "/owners" do
-#   @cat = Cat.new(params)
-#   @owner.save()
-#   erb(:create)
-# end
-#
-# # /photos/:id	GET	show
-#
-# get "/owners/:id/" do
-#   @cat = Cat.find
-# end
-# # /photos/:id	PATCH/PUT	update
