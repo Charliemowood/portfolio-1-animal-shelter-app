2fc2bcbe7ae52705e1eb84fc86e088ff69fa1304
Charlie Wood <charliemowood@gmail.com>
2017-05-09 23:48:21 +0100

    Add create new owner functionality

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -3,13 +3,21 @@ require('sinatra/contrib/all')
 require_relative('../models/owner.rb')
 require_relative('../models/cat.rb')
 
+# INDEX
 get '/owners' do
   @owners = Owner.all()
   erb (:"owner/index")
 end
 
+# NEW
 get '/owners/new' do
-  @cats = Cat.all()
   @owners = Owner.all()
   erb (:"owner/new")
+end
+
+# CREATE
+post '/owners' do
+  @owner = Owner.new(params)
+  @owner.save()
+  erb(:"owner/create")
 end
