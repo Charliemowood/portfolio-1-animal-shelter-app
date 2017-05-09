0ce447a0f9c2f0cf4af725094900587fc95b935d
Charlie Wood <charliemowood@gmail.com>
2017-05-09 20:46:13 +0100

    Add show route on Cat Controller

--- controllers/cat_controller.rb
+++ controllers/cat_controller.rb
@@ -3,18 +3,27 @@ require('sinatra/contrib/all')
 require_relative('../models/cat.rb')
 require_relative('../models/owner.rb')
 
+# INDEX
 get '/cats' do
   @cats = Cat.all()
   erb(:"cat/index")
 end
 
+# NEW
 get '/cats/new' do
   @owners = Owner.all()
   erb(:"cat/new")
 end
 
+# CREATE
 post '/cats' do
   @cat = Cat.new(params)
   @cat.save()
   erb (:"cat/create")
+end
+
+# SHOW
+get '/cats/:id' do
+  @cat = Cat.find(params[:id])
+  erb(:"cat/show")
 end
