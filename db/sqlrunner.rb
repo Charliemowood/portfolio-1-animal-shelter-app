395aedad156378b66f398c855b823b4f7c77ceb2
Charlie Wood <charliemowood@gmail.com>
2017-05-11 11:17:47 +0100

    Set up for Heroku

--- db/sqlrunner.rb
+++ db/sqlrunner.rb
@@ -1,14 +1,18 @@
 require('pg')
 
 class SqlRunner
-
+  # db = PG.connect({dbname: 'animal_shelter', host: 'localhost'})
   def SqlRunner.run(sql)
     begin
-      db = PG.connect({dbname: 'animal_shelter', host: 'localhost'})
+      db = PG.connect({dbname: 'd7b7l1jrc1pecf',
+        host: 'ec2-23-21-224-199.compute-1.amazonaws.com',
+        port: 5432,
+        user: 'rqxbuvaliluyjr',
+        password: 'bd8dac258d4820ba82056b89db735da976b6d349e8f76111f5a8c1416a32cc72'})
       db.exec(sql)
     ensure
       db.close()
     end
   end
-  
+
 end
